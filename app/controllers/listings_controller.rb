96981a129795364c2639966b6aaf63a5542996f1
Norman Silvestre <ngsilvestre@yahoo.com>
2025-07-21 15:11:26 +1000

    listings view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,50 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: %i[ show  update ]
   def index
+    @listings = Listing.all
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    respond_to do |format|
+      if @listing.save
+        format.html { redirect_to @listing, notice: "Listing was successfully created." }
+        format.json { render :show, status: :created, location: @listing }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def show
+  end
+
+    # PATCH/PUT /lists/1 or /lists/1.json
+  def update
+    respond_to do |format|
+      if @listing.update(listing_params)
+        format.html { redirect_to @listing, notice: "listing was successfully updated." }
+        format.json { render :show, status: :ok, location: @listing }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:location, :price, :num_guests, :description, :owner_id)
   end
 end
