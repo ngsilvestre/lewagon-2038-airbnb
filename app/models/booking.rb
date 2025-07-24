c569694f23fa6e8e7da0e71e0adbf4d4fc8c5de1
Quinn Nguyen <quinncien@gmail.com>
2025-07-24 16:31:49 +1000

    added new booking confirm page flow with validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   attribute :status, :integer
   enum status: { pending: 0, confirmed: 1, denied: 2 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
