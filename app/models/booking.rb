9aaefc7b67bba155754ca9ab2dd3c93f35323127
george kettle <georgekettle.gk@gmail.com>
2025-07-25 14:16:22 +1000

    Added validation to booking to ensure start_date before end_date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,14 @@ class Booking < ApplicationRecord
   enum status: { pending: 0, confirmed: 1, denied: 2 }
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  validate :start_date_before_end_date
+
+  private
+
+  def start_date_before_end_date
+    if start_date.present? && end_date.present? && start_date >= end_date
+      errors.add(:start_date, "must be before end date")
+    end
+  end
 end
